57cf61b020ae7b8461fdb902e548ad86b77f44e3
hinalpachori <88702656+hinalpachori@users.noreply.github.com>
2022-07-23 00:16:39 +0530

    Add files via upload

---
+++ importexport/index.js
@@ -0,0 +1,8 @@
+const {add,sub,multi,name}=require('./oper.js')
+//console.log(addition(3,5));
+
+
+console.log(add(5,5))
+console.log(sub(10,3));
+console.log(multi(4,5));
+console.log(name);
---
+++ importexport/oper.js
@@ -0,0 +1,19 @@
+const add=(a,b) =>
+{
+    return a+b;
+}
+
+const sub=(a,b) =>
+{
+    return a-b;
+}
+
+const multi=(a,b) =>
+{
+    return a*b;
+}
+
+const name = "hinal";
+//module.exports.add = add;
+//module.exports.sub=sub;
+module.exports = {add,sub,multi,name}
